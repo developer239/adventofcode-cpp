7047311941277269317a67be5cf1636a56f144e2
michaljarnot <m.jarnot@yahoo.com>
2022-12-01 16:39:29 +0100

    feat: complete day 1 part 1

--- src/day-1/Day1.cpp
+++ src/day-1/Day1.cpp
@@ -1,9 +1,58 @@
 #include <gtest/gtest.h>
 
-int countCalories() {
-  return 2;
+#include <fstream>
+
+template<typename T>
+  void logVectorLines(std::vector<T>& lines) {
+    if constexpr (std::is_same_v<T, std::optional<int>>) {
+      for (auto& line: lines) {
+        if (line.has_value()) {
+          std::cout << line.value() << std::endl;
+        } else {
+          std::cout << " " << std::endl;
+        }
+      }
+    } else {
+      for (auto& line: lines) {
+        std::cout << line << std::endl;
+      }
+    }
+  }
+
+std::vector<std::optional<int>> readInput(const std::string& filename) {
+  std::ifstream file(filename);
+  std::vector<std::optional<int>> input;
+  std::string line;
+
+  while (std::getline(file, line)) {
+    if (line.empty()) {
+      input.emplace_back(std::nullopt);
+    } else {
+      input.emplace_back(std::stoi(line));
+    }
+  }
+  return input;
+}
+
+int countCaloriesPart1() {
+  auto lines = readInput("../../../src/day-1/input.txt");
+
+  std::cout << " " << std::endl;
+
+  std::vector<int> sums = { 0 };
+
+  for (auto line: lines) {
+    if (line.has_value()) {
+      auto lastIndex = sums.size() - 1;
+      sums[lastIndex] += line.value();
+    } else {
+      sums.push_back(0);
+    }
+  }
+
+  return *std::max_element(sums.begin(), sums.end());
 }
 
 TEST(TestCase, FailingTest) {
-  EXPECT_EQ(1, countCalories());
+  EXPECT_EQ(1, countCaloriesPart1());
 }
