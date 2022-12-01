04c4c002e2f2555f424c1acef80929b10512a735
michaljarnot <m.jarnot@yahoo.com>
2022-12-01 16:47:34 +0100

    feat: complete day 1 part 2

--- src/day-1/Day1.cpp
+++ src/day-1/Day1.cpp
@@ -2,23 +2,25 @@
 
 #include <fstream>
 
-template<typename T>
-  void logVectorLines(std::vector<T>& lines) {
-    if constexpr (std::is_same_v<T, std::optional<int>>) {
-      for (auto& line: lines) {
-        if (line.has_value()) {
-          std::cout << line.value() << std::endl;
-        } else {
-          std::cout << " " << std::endl;
-        }
-      }
-    } else {
-      for (auto& line: lines) {
-        std::cout << line << std::endl;
+// TODO: create and move to helper library
+template <typename T>
+void logVectorLines(std::vector<T>& lines) {
+  if constexpr (std::is_same_v<T, std::optional<int>>) {
+    for (auto& line : lines) {
+      if (line.has_value()) {
+        std::cout << line.value() << std::endl;
+      } else {
+        std::cout << " " << std::endl;
       }
     }
+  } else {
+    for (auto& line : lines) {
+      std::cout << line << std::endl;
+    }
   }
+}
 
+// TODO: create and move to helper library
 std::vector<std::optional<int>> readInput(const std::string& filename) {
   std::ifstream file(filename);
   std::vector<std::optional<int>> input;
@@ -34,14 +36,16 @@ std::vector<std::optional<int>> readInput(const std::string& filename) {
   return input;
 }
 
+// TODO: create part1 and part2 functions
 int countCaloriesPart1() {
+  // TODO: pass from arguments
   auto lines = readInput("../../../src/day-1/input.txt");
 
   std::cout << " " << std::endl;
 
-  std::vector<int> sums = { 0 };
+  std::vector<int> sums = {0};
 
-  for (auto line: lines) {
+  for (auto line : lines) {
     if (line.has_value()) {
       auto lastIndex = sums.size() - 1;
       sums[lastIndex] += line.value();
@@ -50,9 +54,15 @@ int countCaloriesPart1() {
     }
   }
 
-  return *std::max_element(sums.begin(), sums.end());
+  std::nth_element(
+      sums.begin(),
+      sums.begin() + 2,
+      sums.end(),
+      std::greater<int>()
+  );
+
+  return sums[0] + sums[1] + sums[2];
 }
 
-TEST(TestCase, FailingTest) {
-  EXPECT_EQ(1, countCaloriesPart1());
-}
+// TODO: more test cases
+TEST(TestCase, FailingTest) { EXPECT_EQ(200945, countCaloriesPart1()); }
