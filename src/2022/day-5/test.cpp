6396e12bd870da964d1e74018152a7dd0489e4cf
michaljarnot <m.jarnot@yahoo.com>
2022-12-05 20:14:30 +0100

    fix: test name

--- src/2022/day-5/test.cpp
+++ src/2022/day-5/test.cpp
@@ -2,8 +2,7 @@
 
 #include "./Day.cpp"
 
-// TODO: rename to TestDay5
-TEST(TestDay05, SuccessCase) {
+TEST(TestDay5, SuccessCase) {
   EXPECT_EQ("CMZ", runPart1("./assets/input-example.txt"));
   EXPECT_EQ("MCD", runPart2("./assets/input-example.txt"));
 }
