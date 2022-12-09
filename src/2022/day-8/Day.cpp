2498b453ace1ee6789a031624b31ca6d9e93b628
michaljarnot <m.jarnot@yahoo.com>
2022-12-09 04:01:38 +0100

    feat: day 8 part 2

--- src/2022/day-8/Day.cpp
+++ src/2022/day-8/Day.cpp
@@ -4,6 +4,8 @@
 #include "src/LogVectorLines.cpp"
 #include "src/ReadInput.cpp"
 
+// TODO: refactor:  🙏🤯create vector for sides, loop and pass directions to check function
+
 void moveUp(int& tempRow, int& tempCol) { tempRow--; }
 void moveDown(int& tempRow, int& tempCol) { tempRow++; }
 void moveLeft(int& tempRow, int& tempCol) { tempCol--; }
@@ -134,4 +136,123 @@ int runPart1(const std::string& filename) {
   }
 
   return visibleTreesCount;
+}
+
+int checkTopPart2(
+    int& tempRow, int& tempCol, const std::vector<std::vector<int>>& grid,
+    const int currenTreeSize
+) {
+  int score = 0;
+  while (tempRow > 0) {
+    moveUp(tempRow, tempCol);
+    score += 1;
+
+    int neighbourTreeSize = grid[tempRow][tempCol];
+    if (neighbourTreeSize >= currenTreeSize) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+int checkLeftPart2(
+    int& tempRow, int& tempCol, const std::vector<std::vector<int>>& grid,
+    const int currenTreeSize
+) {
+  int score = 0;
+  while (tempCol > 0) {
+    moveLeft(tempRow, tempCol);
+    score += 1;
+
+    int neighbourTreeSize = grid[tempRow][tempCol];
+    if (neighbourTreeSize >= currenTreeSize) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+int checkRightPart2(
+    int& tempRow, int& tempCol, const std::vector<std::vector<int>>& grid,
+    const int currenTreeSize
+) {
+  int score = 0;
+  while (tempCol < grid[tempRow].size() - 1) {
+    moveRight(tempRow, tempCol);
+    score += 1;
+
+    int neighbourTreeSize = grid[tempRow][tempCol];
+    if (neighbourTreeSize >= currenTreeSize) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+int checkDownPart2(
+    int& tempRow, int& tempCol, const std::vector<std::vector<int>>& grid,
+    const int currenTreeSize
+) {
+  int score = 0;
+  while (tempRow < grid.size() - 1) {
+    moveDown(tempRow, tempCol);
+    score += 1;
+
+    int neighbourTreeSize = grid[tempRow][tempCol];
+    if (neighbourTreeSize >= currenTreeSize) {
+      return score;
+    }
+  }
+
+  return score;
+}
+
+int runPart2(const std::string& filename) {
+  auto lines = ReadInput<std::string>(filename);
+
+  std::vector<std::vector<int>> trees;
+
+  for (auto& line : lines) {
+    std::vector<int> tree;
+    if (line.has_value()) {
+      for (auto& c : line.value()) {
+        int cInt = c - '0';
+        tree.push_back(cInt);
+      }
+      trees.push_back(tree);
+    }
+  }
+
+  int maxScore = 0;
+  for (int row = 0; row < trees.size(); row++) {
+    for (int col = 0; col < trees[row].size(); col++) {
+      int currenTreeSize = trees[row][col];
+
+      int rowTemp = row;
+      int colTemp = col;
+      int scenicScore1 = checkTopPart2(rowTemp, colTemp, trees, currenTreeSize);
+
+      rowTemp = row;
+      colTemp = col;
+      int scenicScore2 = checkLeftPart2(rowTemp, colTemp, trees, currenTreeSize);
+
+      rowTemp = row;
+      colTemp = col;
+      int scenicScore3 = checkRightPart2(rowTemp, colTemp, trees, currenTreeSize);
+
+      rowTemp = row;
+      colTemp = col;
+      int scenicScore4 = checkDownPart2(rowTemp, colTemp, trees, currenTreeSize);
+
+      int scenicScore = scenicScore1 * scenicScore2 * scenicScore3 * scenicScore4;
+      if (scenicScore > maxScore) {
+        maxScore = scenicScore;
+      }
+    }
+  }
+
+  return maxScore;
 }
--- src/2022/day-8/test.cpp
+++ src/2022/day-8/test.cpp
@@ -3,6 +3,6 @@
 #include "./Day.cpp"
 
 TEST(TestDay8, SuccessCase) {
-  EXPECT_EQ(95437, runPart1("./assets/input.txt"));
-//  EXPECT_EQ(24933642, runPart2("./assets/input-example.txt"));
+  EXPECT_EQ(21, runPart1("./assets/input-example.txt"));
+  EXPECT_EQ(8, runPart2("./assets/input-example.txt"));
 }
