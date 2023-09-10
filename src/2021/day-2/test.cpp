4be36c36295a7603ba5c5c0ab6cb526287621fec
michaljarnot <m.jarnot@yahoo.com>
2023-09-10 22:06:26 +0200

    fix: day 2 test

--- src/2021/day-2/test.cpp
+++ src/2021/day-2/test.cpp
@@ -2,7 +2,7 @@
 
 #include "./Day.cpp"
 
-TEST(TestDay1, SuccessCase) {
+TEST(TestDay2, SuccessCase) {
   EXPECT_EQ(150, runPart1("./assets/input-example.txt"));
   EXPECT_EQ(900, runPart2("./assets/input-example.txt"));
 }
