56789770addb8daef3b0b5a1381528b4632b0e50
michaljarnot <m.jarnot@yahoo.com>
2023-12-02 07:15:48 +0100

    feat: implement part 2

--- src/2023/day-2/Day.cpp
+++ src/2023/day-2/Day.cpp
@@ -77,37 +77,113 @@ int runPart1(const std::string& filename) {
           currentGameConfiguration[splitSet[1]] = std::stoi(splitSet[0]);
         }
 
-        for (auto& defaultConfigurationItem: defaultGameConfiguration) {
+        for (auto& defaultConfigurationItem : defaultGameConfiguration) {
           auto defaultConfigurationLimit = defaultConfigurationItem.second;
-          auto currentConfigurationValue = currentGameConfiguration[defaultConfigurationItem.first];
+          auto currentConfigurationValue =
+              currentGameConfiguration[defaultConfigurationItem.first];
 
-          if(currentConfigurationValue > defaultConfigurationLimit) {
+          if (currentConfigurationValue > defaultConfigurationLimit) {
             invalidGameIds.push_back(gameId);
             break;
           }
         }
       }
 
-      int count = std::count(invalidGameIds.begin(), invalidGameIds.end(), gameId);
-      if(count == 0) {
+      int count =
+          std::count(invalidGameIds.begin(), invalidGameIds.end(), gameId);
+      if (count == 0) {
         validGameIds.push_back(gameId);
       }
     }
   }
 
-  auto idsSum = std::accumulate(validGameIds.begin(),validGameIds.end(), 0);
+  auto idsSum = std::accumulate(validGameIds.begin(), validGameIds.end(), 0);
 
   return idsSum;
 }
 
 int runPart2(const std::string& filename) {
-  auto result = 0;
+  auto totalPowerSum = 0;
+  std::vector<int> invalidGameIds = {};
+  std::vector<int> validGameIds = {};
   auto lines = ReadInput<std::string>(filename);
 
   for (auto line : lines) {
+    // loop lines: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     if (line.has_value()) {
+      auto lineValue = line.value();
+
+      auto gameData = splitString(lineValue, ':');
+
+      auto gameIdString = gameData[0];
+      auto allGameSetsString = gameData[1];
+
+      int gameId;
+      sscanf(gameIdString.c_str(), "Game %d", &gameId);
+
+      auto minimalPossibleCurrentGameConfiguration = emptyGameConfiguration;
+
+      auto gameSets = splitString(allGameSetsString, ';');
+      // loop game sets
+      // {" 3 blue, 4 red"
+      // " 1 red, 2 green, 6 blue"
+      // " 2 green"}
+      for (const auto& gameSetAsString : gameSets) {
+        auto currentGameConfiguration = emptyGameConfiguration;
+
+        // {" 3 blue"
+        // " 4 red"}
+        auto gameSetCubesSets = splitString(gameSetAsString, ',');
+
+        for (auto& cubeSet : gameSetCubesSets) {
+          // "3 blue"
+          ltrim(cubeSet);
+          rtrim(cubeSet);
+
+          auto splitSet = splitString(cubeSet, ' ');
+
+          // "blue"
+          auto key = splitSet[1];
+          // 3
+          auto currentValue = std::stoi(splitSet[0]);
+
+          currentGameConfiguration[key] = currentValue;
+        }
+
+        for (auto& defaultConfigurationItem : defaultGameConfiguration) {
+          auto defaultConfigurationLimit = defaultConfigurationItem.second;
+          auto currentConfigurationValue =
+              currentGameConfiguration[defaultConfigurationItem.first];
+
+          if (currentConfigurationValue > defaultConfigurationLimit) {
+            invalidGameIds.push_back(gameId);
+            break;
+          }
+        }
+
+        // actual part 2
+        for(auto& currentConfigurationItem: currentGameConfiguration) {
+          auto currentItemCurrentValue = currentConfigurationItem.second;
+          auto minimalPossibleValue = minimalPossibleCurrentGameConfiguration[currentConfigurationItem.first];
+
+          if(currentItemCurrentValue > minimalPossibleValue) {
+            minimalPossibleCurrentGameConfiguration[currentConfigurationItem.first] = currentItemCurrentValue;
+          }
+        }
+      }
+
+      auto currentPower = minimalPossibleCurrentGameConfiguration["red"] * minimalPossibleCurrentGameConfiguration["green"] * minimalPossibleCurrentGameConfiguration["blue"];
+      totalPowerSum += currentPower;
+
+      int count =
+          std::count(invalidGameIds.begin(), invalidGameIds.end(), gameId);
+      if (count == 0) {
+        validGameIds.push_back(gameId);
+      } else {
+        std::cout << "game is valid" << std::endl;
+      }
     }
   }
 
-  return result;
+  return totalPowerSum;
 }
