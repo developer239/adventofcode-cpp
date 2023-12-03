b1ba13ec9078ebc5c7991fb759417bdcda9a6b58
michaljarnot <m.jarnot@yahoo.com>
2023-12-03 08:44:40 +0100

    refactor: add isAdjacentToSymbol helper

--- src/2023/day-3/Day.cpp
+++ src/2023/day-3/Day.cpp
@@ -12,6 +12,29 @@ struct Part {
   bool isRealEnginePart = false;
 };
 
+bool isSymbol(char ch) {
+  return !std::isdigit(ch) && ch != '.';
+}
+
+bool isAdjacentToSymbol(int row, int col, int numberLength, const std::vector<std::optional<std::string>>& lines) {
+  static const int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0, 0};
+  static const int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1, 0};
+
+  for (int i = 0; i < numberLength; i++) {
+    for (int d = 0; d < 9; d++) {
+      int newRow = row + dx[d];
+      int newCol = col + i + dy[d];
+
+      if (newRow >= 0 && newRow < lines.size() && newCol >= 0 && newCol < lines[newRow]->length()) {
+        if (isSymbol(lines[newRow]->at(newCol))) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
 int runPart1(const std::string& filename) {
   std::vector<Part> parts = {};
 
@@ -58,130 +81,12 @@ int runPart1(const std::string& filename) {
     }
   }
 
-  // go through all parts
+  // Check if each part is a real engine part using directional scanning
   for (auto& part : parts) {
-    int row = -1;
-    int col = -1;
-    int colLimit = -1;
-    bool isRowInBoundaries = false;
-
-    // check top
-    row = part.row - 1;
-    col = part.column - 1;
-    colLimit = col + part.number.length() + 2;
-
-    isRowInBoundaries = row >= 0 && row < lines.size();
-    if (isRowInBoundaries) {
-      auto line = lines[row];
-
-      if (line.has_value()) {
-        // fix col boundaries
-        if (col < 0) {
-          col = 0;
-        }
-        if (colLimit > line->length() - 1) {
-          colLimit -= 1;
-        }
-
-        for (int i = col; i < colLimit; i++) {
-          auto letter = line->at(i);
-
-          auto isLetterDigit = std::isdigit(letter);
-          auto isPeriod = letter == '.';
-          auto isSymbol = !isPeriod && !isLetterDigit;
-
-          if (isSymbol) {
-            part.isRealEnginePart = true;
-          }
-        }
-      }
-    }
-
-    // check left
-    row = part.row;
-    col = part.column - 1;
-
-    // note: always true
-    isRowInBoundaries = true;
-    if (isRowInBoundaries) {
-      auto line = lines[row];
-
-      if (line.has_value()) {
-        // only if col in boundaries
-        if (col > -1 && col < line->length() - 1) {
-          auto letter = line->at(col);
-
-          auto isLetterDigit = std::isdigit(letter);
-          auto isPeriod = letter == '.';
-          auto isSymbol = !isPeriod && !isLetterDigit;
-
-          if (isSymbol) {
-            part.isRealEnginePart = true;
-          }
-        }
-      }
-    }
-
-    // check right
-    row = part.row;
-    col = part.column + part.number.length();
-
-    // note: always true
-    isRowInBoundaries = true;
-    if (isRowInBoundaries) {
-      auto line = lines[row];
-
-      if (line.has_value()) {
-        // only if col in boundaries
-        if (col > -1 && col < line->length() - 1) {
-          auto letter = line->at(col);
-
-          auto isLetterDigit = std::isdigit(letter);
-          auto isPeriod = letter == '.';
-          auto isSymbol = !isPeriod && !isLetterDigit;
-
-          if (isSymbol) {
-            part.isRealEnginePart = true;
-          }
-        }
-      }
-    }
-
-    // check bottom
-    row = part.row + 1;
-    col = part.column - 1;
-    colLimit = col + part.number.length() + 2;
-
-    isRowInBoundaries = row >= 0 && row < lines.size();
-    if (isRowInBoundaries) {
-      auto line = lines[row];
-
-      if (line.has_value()) {
-        // fix col boundaries
-        if (col < 0) {
-          col = 0;
-        }
-        if (colLimit > line->length() - 1) {
-          colLimit -= 1;
-        }
-
-        for (int i = col; i < colLimit; i++) {
-          auto letter = line->at(i);
-
-          auto isLetterDigit = std::isdigit(letter);
-          auto isPeriod = letter == '.';
-          auto isSymbol = !isPeriod && !isLetterDigit;
-
-          if (isSymbol) {
-            part.isRealEnginePart = true;
-          }
-        }
-      }
-    }
+    part.isRealEnginePart = isAdjacentToSymbol(part.row, part.column, part.number.length(), lines);
   }
 
   auto sumOfAllParts = 0;
-
   for (const auto part : parts) {
     if (part.isRealEnginePart) {
       sumOfAllParts += std::stoi(part.number);
