a4a09cf618bca34291a65c9b4daa1a7589b73b95
michaljarnot <m.jarnot@yahoo.com>
2023-12-20 20:38:24 +0100

    fix: test day 6

--- src/2023/day-6/test.cpp
+++ src/2023/day-6/test.cpp
@@ -3,6 +3,6 @@
 #include "./Day.cpp"
 
 TEST(TestDay23_6, SuccessCase) {
-  EXPECT_EQ("TODO", runPart1("./assets/input.txt"));
-  EXPECT_EQ("TODO", runPart2("./assets/input.txt"));
+  EXPECT_EQ(345015, runPart1("./assets/input.txt"));
+  EXPECT_EQ(42588603, runPart2("./assets/input.txt"));
 }
