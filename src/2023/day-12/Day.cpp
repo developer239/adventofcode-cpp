a0edf84f008e22bcf9c4b43d3dd344ca61a102f3
michaljarnot <m.jarnot@yahoo.com>
2023-12-23 02:25:35 +0100

    feat: prepare day 12

---
+++ src/2023/day-12/Day.cpp
@@ -0,0 +1,12 @@
+#include <numeric>
+
+#include "src/LogVectorLines.cpp"
+#include "src/ReadInput.cpp"
+
+int runPart1(const std::string& filename) {
+  return 0;
+}
+
+int runPart2(const std::string& filename) {
+  return 0;
+}
---
+++ src/2023/day-12/test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "./Day.cpp"
+
+TEST(TestDay23_12, SuccessCase) {
+  EXPECT_EQ(0, runPart1("./assets/input.txt"));
+  EXPECT_EQ(0, runPart2("./assets/input.txt"));
+}
