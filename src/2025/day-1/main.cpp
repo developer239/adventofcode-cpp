581c91f5d89aaf6dff2cb5ddba96593e2a285b99
michaljarnot <m.jarnot@yahoo.com>
2025-12-09 21:12:32 +0100

    refactor: polish code

--- src/2025/day-1/main.cpp
+++ src/2025/day-1/main.cpp
@@ -3,11 +3,11 @@
 #include "Day.cpp"
 
 int main() {
-  auto result1 = runPart1("./assets/input.txt");
-  // auto result2 = runPart2("./assets/input-example-1.txt");
+  // auto result1 = runPart1("./assets/input.txt");
+  auto result2 = runPart2("./assets/input-example-1.txt");
 
-  std::cout << "Part 1: " << result1 << std::endl;
-  // std::cout << "Part 2: " << result2 << std::endl;
+  // std::cout << "Part 1: " << result1 << std::endl;
+  std::cout << "Part 2: " << result2 << std::endl;
 
   return 0;
 }
